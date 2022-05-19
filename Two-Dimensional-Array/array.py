fb0ab81f96c6698a9b64bfacc0d042c2940fe16c
Mofiro Jean <mofirojean@gmail.com>
2022-05-19 15:16:34 +0100

    more modification

--- Two-Dimensional-Array/array.py
+++ Two-Dimensional-Array/array.py
@@ -53,3 +53,49 @@ class _ArrayIterator:
             return entry
         else:
             raise StopIteration
+
+
+# Implementation of the Array2D ADT using arrays of arrays
+class Array2D:
+    """ Creates a 2-D array of size numRows x numCols"""
+    def __init__(self, numRows, numCols):
+        # Creates a 1-D to store an array reference for each row.
+        self._theRows = Array(numRows)
+
+        # Create the 1-D arrays for each row of the 2-D array.
+        for i in range(numRows):
+            self._theRows[i] = Array(numCols)
+
+    # Returns the number of rows in the 2-D array.
+    def numRows(self):
+        return len(self._theRows)
+
+    # Returns the number of columns in the 2-D array.
+    def numCols(self):
+        return len(self._theRows[0])
+
+    # Clears the array by setting every element to the given value.
+    def clear(self, value):
+        for row in range(self.numRows()):
+            for i in range(self.numCols()):
+                self._elements[i] = value
+
+    # Gets the content of the elements at the position  [i, j]
+    def __getitem__(self, ndxTuple):
+        assert len(ndxTuple) == 2, "Invalid number of array subscript"
+        row = ndxTuple[0]
+        col = ndxTuple[1]
+        assert (0 <= row < self.numRows()) and (0 <= col < self.numCols()), "Array subscripts out of range"
+        theIdArray = self._theRows[row]
+        return theIdArray[col]
+
+    # Set the contents of the element at position [i, j] to value.
+    def __setitem__(self, ndxTuple, value):
+        assert len(ndxTuple) == 2, "Invalid number of array subscript"
+        row = ndxTuple[0]
+        col = ndxTuple[1]
+        assert (0 <= row < self.numRows()) and (0 <= col < self.numCols()), "Array subscripts out of range"
+        theIdArray = self._theRows[row]
+        theIdArray[col] = value
+
+
